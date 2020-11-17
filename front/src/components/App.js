863765868520754c07c810dbc98c83f4aac48147
Ely Lam <eydelamb14@gmail.com>
2020-11-17 13:31:16 -0800

    Update App.js

--- front/src/components/App.js
+++ front/src/components/App.js
@@ -168,7 +168,7 @@ function App() {
         <br/>
         <div className="row">
           <footer>
-            All character and image rights to Pokemon, Nintendo<br/>
+            All character and image rights to Pokémon, Nintendo<br/>
             Webpage created by Alex Moeller and Ely Lam 2020{" "}
             <img
               src="./images/pokeball.png"
